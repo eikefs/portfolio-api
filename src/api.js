78467c7a0e0ad1d992045bbbc126e8f45035efd5
Eike Fabrício da S <66657952+eikefs@users.noreply.github.com>
2020-06-22 14:55:40 -0300

    Update api.js

--- src/api.js
+++ src/api.js
@@ -4,7 +4,6 @@ const axios = require('axios');
 
 const instance = axios.create({
     baseURL: process.env.REQUEST_URL,
-    timeout: 1000,
     headers: {
         Authorization: `token ${process.env.OAUTH_TOKEN}`
     }
